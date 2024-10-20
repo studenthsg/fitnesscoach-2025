382a8f14c7b4de724402bca61fe8cf78ed4b0b09
studenthsg <fynn.lenzlinger@student.unisg.ch>
2024-10-20 10:58:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("Meal Planer 🍗")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+number = st.slider("How much fun is this project", 0, 100)
