8aac0808043dcec59a0d5ff805841e01a6312f49
studenthsg <fynn.lenzlinger@student.unisg.ch>
2024-11-27 11:12:51 +0100

    Update Users.py

--- pages/Users.py
+++ pages/Users.py
@@ -5,4 +5,9 @@ st.title("Users")
 st.markdown("Users")
 st.sidebar.header("Users")
 
+st.set_page_config(
+    page_title="Account",
+    page_icon="🌶️",
+)
+
 st.write("You have entered", st.session_state["my_input"])
