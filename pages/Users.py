2067659f9c572dfe38623108d3b172b282280d52
studenthsg <fynn.lenzlinger@student.unisg.ch>
2024-11-27 11:15:52 +0100

    Update Users.py

--- pages/Users.py
+++ pages/Users.py
@@ -1,14 +1,7 @@
 import streamlit as st
 
-
-st.title("Users")
 st.markdown("Users")
 st.sidebar.header("Users")
-
-st.set_page_config(
-    page_title="Account",
-    page_icon="🌶️",
-)
 
 st.write("You have entered", st.session_state["my_input"])
 
