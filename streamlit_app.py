7d93cb4d01fe5b1dd0ac20386b9dd92bd7dc9177
studenthsg <fynn.lenzlinger@student.unisg.ch>
2024-11-27 12:44:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,14 @@ import requests
 
 from st_pages import Page, show_pages, add_page_title
 
-# Optional -- adds the title and icon to the current page
-add_page_title()
-
-# Specify what pages should be shown in the sidebar, and what their titles 
-# and icons should be
-show_pages(
-    [
-        Page("streamlit_app.py", "Home", "🏠"),
-    ]
+st.set_page_config(
+    page_title="Home",
+    page_icon="🏠"
 )
+
+st.title("Meal Planner 🍗")
+st.sidebar.header("Home")
+st.sidebar.success("Select a page above.")
 
 st.title("Welcome to the Meal Planner")
 st.sidebar.title("Meal Planner App")  
