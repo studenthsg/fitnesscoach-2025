f97d821e1428aa17c3058faeba216950fd365fb2
studenthsg <fynn.lenzlinger@student.unisg.ch>
2024-11-27 11:33:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,6 @@ st.markdown("Hello, and welcome! You’ve just discovered a space where the ordi
 
 st.markdown("Whether you’ve come to explore, to create, or simply to be inspired, this is a home designed for YOU. Here, innovation meets imagination, and every click brings you closer to something remarkable.")
 
-st.markdown(rainbow:[Let this be your sanctuary of discovery, a place where every moment feels special, and every experience is tailored to leave a lasting impression.])
+st.markdown("rainbow:[Let this be your sanctuary of discovery, a place where every moment feels special, and every experience is tailored to leave a lasting impression.]")
 
 st.markdown("Relax, explore, and enjoy—because this isn’t just a website. It’s the start of something truly unforgettable.")
