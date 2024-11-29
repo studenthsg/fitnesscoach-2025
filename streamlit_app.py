b34e80877a374fdaac65c5525297fdc94a73437b
studenthsg <fynn.lenzlinger@student.unisg.ch>
2024-11-29 12:38:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -217,7 +217,7 @@ if page == "Weekly Planner":
 elif page == "My Account":
     st.title("My Account 🧑‍💻")
 
-   import supabase
+    import supabase
 
     st.title("Account")
 
@@ -225,11 +225,3 @@ elif page == "My Account":
     SUPABASE_URL = "https://qbnmfdcuzeghmyobcnhi.supabase.co"
     SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFibm1mZGN1emVnaG15b2JjbmhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI2OTg5NzcsImV4cCI6MjA0ODI3NDk3N30.FXophJC6_BilPfwJ8G1oI9Z_8UBqD9uf2UX0OgY3i00"
     supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-
-    def insert_user(username: str, password: str):
-      try:
-        response = supabase.table("users").insert({"username": username,
-                    "password": password}).execute()
-        return response.data
-      except Exception as e:
-        return e
