b3832a26dec741e475eae91febfa329d3269d2be
studenthsg <fynn.lenzlinger@student.unisg.ch>
2024-11-30 14:45:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -249,14 +249,6 @@ elif page == "Weekly Planner":
 elif page == "My Account":
     st.title("My Account 🧑‍💻")
 
-    import supabase
-    from supabase import create_client, Client
-
-    SUPABASE_URL = "https://qbnmfdcuzeghmyobcnhi.supabase.co"
-    SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFibm1mZGN1emVnaG15b2JjbmhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI2OTg5NzcsImV4cCI6MjA0ODI3NDk3N30.FXophJC6_BilPfwJ8G1oI9Z_8UBqD9uf2UX0OgY3i00"
-
-    supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-
     # Initialize session state for login
     if "logged_in" not in st.session_state:
         st.session_state.logged_in = False
