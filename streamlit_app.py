7a5570fa222da79353c258f4c095b4fd9d8e2166
studenthsg <fynn.lenzlinger@student.unisg.ch>
2024-12-06 13:27:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -296,16 +296,9 @@ import streamlit as st
 
 @st.cache_resource
 def train_model():
-    # Query data from Supabase
+    # Get data from Supabase - the data contains weight, height, gender, age and the daily calories of 1000 persons. The data has been created with ChatGTP and is fictitious. 
     try:
         response = supabase.table("calories").select("weight, height, gender, age, daily_caloric_needs").execute()
-
-        # Now check the structure of the response
-        if hasattr(response, 'data'):
-            data = pd.DataFrame(response.data)  # Convert the data to DataFrame
-            if data.empty:
-                st.error("No data found in the calories table.")
-                return None
 
             # Preprocessing: Convert Gender to numeric using LabelEncoder
             data['gender'] = LabelEncoder().fit_transform(data['gender'])
