c1e3cecdb3943e6a9f0b1e5f23be0dbaa7da307e
studenthsg <fynn.lenzlinger@student.unisg.ch>
2024-12-06 13:36:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,6 @@ API_URL = "https://api.spoonacular.com/recipes/complexSearch"
 RECIPE_INFO_URL = "https://api.spoonacular.com/recipes/{id}/information"  # URL to get recipe details
 API_KEY = "a636f339cbdb4409ae46bb47e0c35577"
 
-base="light"
-primaryColor="#34eb40"  # Optional: Set a primary color
-backgroundColor="#ffffff"  # Optional: Set the main app background color
-secondaryBackgroundColor="#90EE90"  # Light green sidebar color
-textColor="#000000"
-
 # Initialize session state
 if "saved_recipes" not in st.session_state:
     st.session_state["saved_recipes"] = {"Breakfast": [], "Lunch": [], "Dinner": []}
